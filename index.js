e5873e7b7458fd204b9cc47fc1a37836181bc586
chodavarapu sowdhamini <94843433+sowdhamini-buildbot@users.noreply.github.com>
2022-03-09 15:47:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,24 @@ console.log(x)
 const x;  // Error! Missing initializer in const declaration.
 x = 5;
 console.log(x)
+
+//identifier:variables and constants are usually call identifiers.
+//naming roles of identifers : identifiers consists of letters,numbers, underscore(_) and the dollar sign($)
+
+//valid
+const a = 'hello';
+const _a = 'hello';
+const $a = 'hello';
+//unicode character are allowed
+
+//identifier cannot be reserve word
+//invalid
+const new = 5; // Error! new is a keyword.
+
+//Identifier names cannot start with numbers
+//invalid
+const 1a = 'hello'; // this gives an error
+
+
+//literals: literals means you are providing a value directly in the program . literals is just the way to create identifier with the value.
+let goal = "be happy"//literal-behappy 
